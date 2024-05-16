ac8ada0cfa8164ef38215316d79a1929e5673844
Cường Phạm <>
2024-05-16 13:29:07 +0700

    test use redis

--- src/app.js
+++ src/app.js
@@ -15,6 +15,10 @@ app.use(
     extended: true,
   })
 );
+// test pub.sub redis
+require("./test/inventory.test");
+const productTest = require("./test/product.test");
+productTest.purchaseProduct("product:001", 10);
 // init db
 require("./dbs/init.mongodb");
 // const { checkOverLoad } = require("./helpers/check.connect");
